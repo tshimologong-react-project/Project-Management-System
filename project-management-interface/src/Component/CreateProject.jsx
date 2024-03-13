a4ba32c66a336e6d2ef0dc728358fd83a161fa6d
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-03-13 09:00:48 +0200

    edited and updated the project creation page

--- project-management-interface/src/Component/CreateProject.jsx
+++ project-management-interface/src/Component/CreateProject.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 // import axios from 'axios'; // Import axios for making HTTP requests
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
-import "./styles/stylesheet.css"
+
 
 
 function CreateProject() {
@@ -31,19 +31,26 @@ function CreateProject() {
         <div className="project-wrapper">
           <Navbar />
           <div className="create-project-section">
-            <div className="Create_Project_Header"><h1>Create Project</h1></div>
-            <div className="Create_Project_Para"><p>Lorem ipsum dolor sit amet consectetur. Volutpat est egestas elementum accumsan lorem et. Consequat condimentum dui enim natoque molestie volutpat sollicitudin tellus semper. Dolor nibh mauris sed sed tempor. Duis amet mauris morbi neque.</p></div>
-            <div className="Form_Title_section">
-              <p>Title</p>
-              <input type="text" name="title" value={contact.title} />
-            </div>
-            <div className="Form_Description_section">
-              <p>Description</p>
-              <textarea name="description" value={contact.description} />
-            </div>
-            <div className="create_project_button_section">
-              <button >Create Project</button>
-            </div>
+              <div className="container">
+                <h1 className="Create_Project_Header">Create Project</h1>
+                <p className="Create_Project_Para">Lorem ipsum dolor sit amet consectetur. Volutpat est egestas elementum accumsan lorem et. Consequat condimentum 
+                dui enim natoque molestie volutpat sollicitudin tellus semper. Dolor nibh mauris 
+                sed sed tempor. Duis amet mauris morbi neque.</p>
+                <form action="">
+                  <div className="Form_Title_section">
+                    <p>Project title</p>
+                    <input placeholder='Write project title' type="text" name="title" value={contact.title} />
+                  </div>
+                  <div className="Form_Description_section">
+                    <p>Description</p>
+                    <textarea placeholder='Describe your project' name="description" value={contact.description} />
+                  </div>
+                  <div className="create_project_button_section">
+                    <button >Create Project</button>
+                  </div>
+                </form>
+                
+              </div>
           </div>
         </div>
       </div>
