d253f13e32b56d772e1e3473d5a69bc85017ceba
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-03-16 13:34:38 +0200

    Delete backend directory

--- backend/src/main/java/com/algoExpert/demo/Repository/AssigneesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.algoExpert.demo.Repository;
-
-import com.algoExpert.demo.Entity.Assignee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssigneesRepository extends JpaRepository<Assignee, Integer> {
-
-}
