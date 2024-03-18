c58cf8617c7431ef2e415165af6a60a31caa6ee2
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-03-18 09:58:54 +0200

    created table entity again

--- backend/src/main/java/com/algoExpert/demo/Repository/TableRepository.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/TableRepository.java
@@ -1,11 +1,11 @@
 package com.algoExpert.demo.Repository;
 
 
-import com.algoExpert.demo.Entity.Tables;
+import com.algoExpert.demo.Entity.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TableRepository extends JpaRepository<Tables, Integer> {
+public interface TableRepository extends JpaRepository<Table, Integer> {
 
 }
